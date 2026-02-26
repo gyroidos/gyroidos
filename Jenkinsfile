gyroidos/gyroidos

commit c02d3d7bd7ed0ace80caefa086964540f27959cd

Jenkinsfile: added random delay before ssh mirror check
If SYNC_MIRRORS is set a test of the ssh agent is performed by accessing
the mirror host by "ssh -v ${env.MIRRORHOST} "ls -al /yocto_mirror"
This may lead to following error:

  debug1: kex_exchange_identification: banner line 0: Exceeded MaxStartups
  kex_exchange_identification: Connection closed by remote host
  Connection closed by <MIRRORHOST IP> port 22
  script returned exit code 255

A random delay should fix this.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

Jenkinsfile
@@ -230,6 +230,7 @@ pipeline {
 												echo "Test ssh agent"
 												cat /home/builder/.ssh/ci_known_hosts
 												cat /home/builder/.ssh/config
+												sleep $((RANDOM % 2)).$((RANDOM));
 												ssh -v ${env.MIRRORHOST} "ls -al /yocto_mirror"
 											"""
 										}

commit 90bcdff9e5ca49d0ef61599aa510f8c470e1d567

Jenkinfile: Properly escape dollar sign
Fix pipeline failure by properly escaping dollar sign added in gyroidos/gyroidos#89.

Fixes: c02d3d7bd7ed ("Jenkinsfile: added random delay before ssh mirror check")
Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>
[michael.weiss: added Fixes tag]
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

Jenkinsfile
@@ -230,7 +230,7 @@ pipeline {
 												echo "Test ssh agent"
 												cat /home/builder/.ssh/ci_known_hosts
 												cat /home/builder/.ssh/config
-												sleep $((RANDOM % 2)).$((RANDOM));
+												sleep \$((RANDOM % 2)).\$((RANDOM))
 												ssh -v ${env.MIRRORHOST} "ls -al /yocto_mirror"
 											"""
 										}